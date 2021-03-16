125f4cd9bd66004433a894114980a6cf97f5bde7
eypaksu <eyup.aksu@yahoo.com>
2021-03-16 17:24:55 +0000

    PRMT-1673 [EA/JG] Change log group arn name

--- stacks/stepfunction-spike/word-count.tf
+++ stacks/stepfunction-spike/word-count.tf
@@ -2,20 +2,30 @@ data "aws_ecr_repository" "word_count" {
   name = var.wordcount_repo_name
 }
 
-resource "aws_cloudwatch_log_group" "word_count_example" {
-  name = "/ecs/${var.environment}-word-count-example"
+resource "aws_cloudwatch_log_group" "word_count" {
+  name = "/ecs/${var.environment}-word-count"
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.environment}-word-count-example"
+      Name = "${var.environment}-word-count"
     }
   )
+}
+
+resource "aws_iam_policy" "ecs_execution" {
+  name   = "${var.environment}-ecs-data-pipeline-execution"
+  policy = data.aws_iam_policy_document.ecs_execution.json
 }
 
 resource "aws_iam_role" "ecs_execution" {
   name               = "${var.environment}-registrations-word-count-task"
   description        = "ECS task role for launching word count"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = aws_iam_policy.ecs_execution.arn
 }
 
 resource "aws_iam_role" "word_count" {
@@ -34,7 +44,7 @@ resource "aws_ecs_task_definition" "word_count" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = aws_cloudwatch_log_group.word_count_example.name
+          awslogs-group         = aws_cloudwatch_log_group.word_count.name
           awslogs-region        = var.region
           awslogs-stream-prefix = var.wordcount_image_tag
         }
@@ -49,7 +59,7 @@ resource "aws_ecs_task_definition" "word_count" {
   tags = merge(
     local.common_tags,
     {
-      Name = "${var.environment}-word-count-example"
+      Name = "${var.environment}-word-count"
     }
   )
   execution_role_arn = aws_iam_role.ecs_execution.arn
@@ -77,5 +87,40 @@ resource "aws_security_group_rule" "word_count" {
   to_port     = 0
   protocol    = "-1"
   description = "Unrestricted egress"
+}
+
+data "aws_iam_policy_document" "ecs_execution" {
+  statement {
+    sid = "GetEcrAuthToken"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    sid = "DownloadEcrImage"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage"
+    ]
+    resources = [
+      data.aws_ecr_repository.word_count.arn
+    ]
+  }
+
+  statement {
+    sid = "CloudwatchLogs"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.word_count.arn}:*"
+    ]
+  }
 }
 
