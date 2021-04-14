f783816971789ecc644826245aff8c833a9083ba
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-04-14 11:13:32 +0100

    PRMT-1795 [JG/EA] set region in the tasks script instead of backend.tf

--- stacks/base-networking/backend.tf
+++ stacks/base-networking/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket  = "prm-gp2gp-terraform-state-${var.environment}"
-    dynamodb_table = "prm-gp2gp-terraform-table"
-    region  = "eu-west-2"
     encrypt = true
   }
 }
--- stacks/data-pipeline/backend.tf
+++ stacks/data-pipeline/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket  = "prm-gp2gp-terraform-state"
-    dynamodb_table = "prm-gp2gp-terraform-table"
-    region  = "eu-west-2"
     encrypt = true
   }
 }
