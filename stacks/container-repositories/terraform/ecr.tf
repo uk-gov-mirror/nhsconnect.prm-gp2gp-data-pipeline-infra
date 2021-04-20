6d4760d38f046d7eb34287dc374262efc8c739f3
Jake Newman <jacob.newman1@nhs.net>
2021-04-20 17:16:48 +0100

    PRMT-1795 [JN/JF] Append env to ods downloader parameter name

--- stacks/container-repositories/terraform/ecr.tf
+++ stacks/container-repositories/terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ods_downloader" {
-  name = "registrations/ods-downloader"
+  name = "registrations/ods-downloader-${var.environment}"
 
   tags = {
     Name        = "Ods data downloader"
